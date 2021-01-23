69bd73e984654ef453dc8fd616e53a2ad37f4796
Dexter <dex429.dc@gmail.com>
2021-01-22 22:30:09 -0500

    commit tree bae3ab23d50daa18e04c06e56486e64006a73c9e

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,9 @@ require 'pry'
 class Song
 
   extend Memorable::ClassMethods
+  extend Findable
+  include Paramable
+  
 
   attr_accessor :name
   attr_reader :artist
@@ -13,9 +16,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -33,7 +36,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
